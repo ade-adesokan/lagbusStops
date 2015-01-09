6207ac98bb12b8dcb3542602501f4000b5df28c9
andela-aadesokan <adewunmi.adesokan@andela.co>
2015-01-09 11:13:23 +0100

    accessControl

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ var router = require('./routes/index');
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+};
+
+
+
 app.set('port', (process.env.PORT || 3000));
+
+app.use(allowCrossDomain);
 
 
 app.use('/api', router);
